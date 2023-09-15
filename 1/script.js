66cc15e9a3b4fd9df282a27a5da42b7f16ecf27b
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-09-15 20:33:20 +0200

    add notes

--- 1/script.js
+++ 1/script.js
@@ -1,11 +1,22 @@
-console.log("Helló világ!");
-
+// INFORMÁCIÓK
 // JS doksi: https://developer.mozilla.org/en-US/docs/Web/JavaScript
 // GitHub: https://github.com/Valentinusz/webprog-2023-24-1-5
 // Canvas: https://canvas.elte.hu/courses/38953
 
+// JS
+// többparadigmájú (főleg objektumelvű, mostanában sok funkcionális elem)
+// dinamikusan típusos (változók típusát az azokban tárolt érték típusa határozza meg)
+// gyengén típusos (nyelv sok konverziót végez pl. 5 + 'a' = '5a')
+// böngésző konzol (inspect element (shortcut: F12) -> konzol) REPL környezet
+// C alapú nyelv, vezérlési szerkezetek nagyrészt megegyeznek
+
+// Feladatsor: http://webprogramozas.inf.elte.hu/#!/subjects/webprog-pti/gyak/01
+// 1. Írjuk ki konzolra, hogy Helló Világ
+console.log("Helló világ!");
+
 // 2. Készítsd el a Fahrenheitből Celsius fokba átalakító függvényt! 5/9 * (F-32)
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+// Függvényeket több módon is meg tudunk adni
+// Nem teljesen ekvivalensek
 function f2C(F) {
     return 5/9 * (F-32);
 }
@@ -24,6 +35,7 @@ console.log(
 );
 
 //arrow function
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 const f2C3 = (F) => {
     return 5/9 * (F-32);
 }
@@ -32,7 +44,8 @@ console.log(
     f2C3(40)
 );
 
-// arrow function (1 soros)
+// arrow function expression
+// (ha nem írunk kapcsos zárójelet, akkor a nyíl jobb oldalán szereplő kifejezés implicit return-ölve lesz)
 const f2C4 = F => (5/9 * (F-32));
 
 console.log(
@@ -52,15 +65,40 @@ console.log(
 // Függvény vége
 function lnko(a, b) {
     if (a < b) {
-        // destructuring
-        [a, b] = [b, a];
+        // const konstans változó
+        // const TEMP = a;
+        // b = a;
+        // a = TEMP;
+        // array destructuring
+        // a váltózó értéke a jobb oldali tömb első eleme
+        // b változó értéke a tömb második eleme stb.
+        [a, b] = [b, a]
     }
+    // let sima változó létrehozása
     let maradek = a % b;
     while(maradek > 0) {
-        // utanánézni ez pontosan hogy működik
-        [a, b, maradek] = [b, maradek, a % b]
+
+        // először jön létre a jobb oldali tömb, szóval, ami itt nem jó, mert a maradékot már a frissített értékekből
+        // akarjuk számolni
+        [a, b] = [b, maradek]
+        maradek = a % b
     }
     return b;
+}
+
+// 8.
+function lkkt(a, b) {
+    let x = a;
+    let y = b;
+
+    while(x !== y) {
+        if (x < y) {
+            x += a;
+        } else if (x > y) {
+            y += b;
+        }
+    }
+    return x;
 }
 
 console.log(
@@ -69,18 +107,20 @@ console.log(
 
 // tömbfüggvények
 // magasabb rendű függvények
-// progarmozási tételeket jól lehet implementálni
+// programozási tételeket jól lehet implementálni
 
 // 11. Egy számsorozatban keress meg egy negatív számot.
 // keresés
 const numbers = [2, 5, 9, -1, 3, 5, 1, 2, 1];
 
-// find
+// predikátum: logikai értéket visszaadó függvény
+// find első predikátumnak megfelelő elemet adja vissza
+
 console.log(
     numbers.find(number => number < 0)
 );
 
-// findIndex
+// findIndex első predikátumnak megfelelő elem indexét adja vissza
 console.log(
     numbers.findIndex(number => number < 0)
 );
@@ -89,15 +129,23 @@ console.log(
 // megszámlálás
 // 2 módon is megoldható
 
-// kiválogatás
+// kiválogatás + hosszlekérés
 console.log(
     numbers.filter(number => number % 2 == 0).length
 )
 
+// reduce
+// két paramétert vár
+// 1. függvény, aminek legalább egy két paramétere van itt: (accumulator, current)
+// 2. accumulator változó kezdeti értéke
+// reduce feldolgozza egyesével a tömb elemeit, minden feldolgozott elem után
+// az accumulator változó értékét minden elem feldolgozásakor, az 1. paraméterként
+// átadott függvény visszatérése értékére állítja
 console.log(
     numbers.reduce((acc, curr) => (curr % 2 === 0 ? acc+1 : acc), 0)
 );
 
+// jobban kifejtve a paraméterként átadott függvényt
 console.log(
     numbers.reduce((acc, curr) => {
         if (curr % 2 === 0) {
@@ -111,13 +159,14 @@ console.log(
 // 13. Válogasd ki azokat a számokat, amelyek mindkét szomszédjuktól egy előre bekért értéken belül térnek el.
 const limit = 2;
 
+// a paraméterként átadott callback function-ök szignatúrája bővebb is lehet
+// a pontos szignatúrákat MDN-n érdemes megnézni
 console.log(
     numbers.filter((currentValue, currentIndex, array) => {
-        // undefinedon aritmetikai művelet végzése = NaN (not a number)
-        // logika kiértékelés hamis
-        // összahasonlítás más számmal szintén hamis
-        return Math.abs(currentValue - array[currentIndex - 1]) < limit &&
-        Math.abs(currentValue - array[currentIndex + 1]) < limit
+        // túlindexelés -> undefined
+        // undefined + aritmetikai művelet végzése -> NaN (not a number)
+        // NaN összahasonlítása más számmal hamis
+        return Math.abs(currentValue - array[currentIndex - 1]) < limit && Math.abs(currentValue - array[currentIndex + 1]) < limit;
     })
 );
 
@@ -126,6 +175,7 @@ const matrix = [[1, 2, 3], [2, 3, 4], [6, 4, 2]];
 // eldöntés
 // minden: every
 // létezik: some
+// mindkét függvény predikátumot vár
 
 console.log(
     matrix.every(row => row.every(number => number % 2 == 0))
@@ -140,7 +190,7 @@ console.log(
 // megengedőbb JSON (JavaScript Object Notation)
 const obj = {
     mezo1: 12,
-    'mezo2': 'alma',
+    'mezo2': 'alma', //így is lehet
 
     metodus() {
         console.log(this.mezo2);
@@ -150,6 +200,7 @@ const obj = {
         console.log(this.mezo1);
     },
 
+    // ez furán működik, akit érdekel utánaolvashat MDN-en
     metodus3: () => {
         console.log(this.mezo1);
     }
@@ -165,7 +216,7 @@ console.log(
 
 obj.metodus();
 obj.metodus2();
-obj.metodus3();
+obj.metodus3(); // undefined
 
 class Movie {
     constructor(title, length, category, directors) {
@@ -173,7 +224,7 @@ class Movie {
         this.length = length;
         this.category = category;
         // spread operator const a = [a, b ,c] ...a = a,b,c
-        // sekély másolat
+        // shallow copy
         this.directors = [...directors];
     }
 }
@@ -188,13 +239,16 @@ const movies = [
 // a,
 console.log(movies);
 
+// forEach elemek feldolgozása, nem jön létre új tömb
 movies.forEach(movie => console.log(movie));
 movies.forEach(console.log);
 
+// táblázatos formátumú konzolra írás
 console.table(movies);
 
 // b,
 console.table(
+    // object destructuring, az objektum megyegyező nevű mezőit a különb változókba menti
     movies.filter(({directors}) => directors.length > 1)
 );
 
@@ -218,12 +272,13 @@ console.log(
 
 // b,
 // másolás tétel
-// valamilyen logika mentén egy új tömböt hoz létre
+// a megadott függvényben szereplő logika mentén egy új tömböt hoz létre
 console.log(
     temperatures.map(temperature => temperature + "C")
 );
 
 // c,
+// -Infinity minden számnál kisebb 
 console.log(
     temperatures.reduce((max, currentTemp) => (currentTemp > max ? currentTemp : max), -Infinity)
 )
