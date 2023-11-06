a3001fc9a1c1450f4c9b47f4ff8efb53e6ea7f4b
Valentinusz <68827048+Valentinusz@users.noreply.github.com>
2023-11-06 20:32:32 +0100

    clean up gyak7

--- 07/basics.php
+++ 07/basics.php
@@ -1,97 +1,310 @@
 <?php
+// szerveroldali webprogramozás célja: dinamikus tartalom előállítása
+// tartalom sokféle lehet
+// legegyszerűbb egész HTML-t legenerálni is válaszként visszaadni (elsődlegesen ezzel fogunk foglalkozni)
+// JSON, XML és egyéb félig strukturált adatot is vissza lehet adni (ezt majd kliensoldalon kell kezelni)
 
-//
-/**
- *
- */
+// PHP
+// dinamikusan gyengén típusos többparadigmájú (procedurális múlt, teljesértékű OOP, first-class függvények)
+// JS-hez sok tekintetben hasonló (viszont ; kötelező)
+// php biztosít fejlesztői szervert: ez a PHP telepítése után a php -S <address>:<port> paranccsal indítható el
+// pl. php -S localhost:8000
+// többféle verzió (egészen nagy különbségek), mi 8.0 fölötti verziót fogunk használni
+// doksi: https://www.php.net/manual/en/
 
-/**
- *
- */
+// PHP működése
+// kliens HTTP kérést küldd a webszervernek, ha létezik az erőforrás .php kiterjesztéssel a common gateway interface-en
+// keresztül végrehajtásra kerül a PHP script, amit a script a standard outputra ír lesz a kliensnek adott HTTP válasz
 
-#
+// PHP nyelv jellemzői
 
-$intNum = 5;
-$sep = 5_000_000;
-$floatNum = 3.14;
+# Kommentek
 
-$str = 'asdasd';
-$str2 = "asdasd";
-$str3 = "$str";
-$str4 = $str . $str2;
+# (Perl-féle) komment
 
-$boolean = true;
+// komment
 
-# array
-$index_array = [1, 2, 3, 4, 5];
+/*
+ * többsoros
+ * komment
+*/
 
 
-var_dump($index_array);
 
-echo '<br>';
+// Kiíratás
+echo "asd";
 
-var_export($index_array);
 
+
+# Változók
+
+// dinamikusan típusos nyelv
+// $ prefix (így könnyű stringbe helyettesíteni)
+$valtozo = "asd";
+const asd = 'konstans';
+
+
+# Debug
+
+// egy változó információinak kiiratása a standard outputra
+var_dump($valtozo);
+
+// "olvashatóbb formában"
 echo '<pre>';
-print_r($index_array);
+print_r($valtozo);
 echo '</pre>';
 
-$associative_array = ['elso' => 1, 'masodik' => 2, 'harmadik' => "asdasd"];
+// olyan formában ami valid PHP kód
+var_export($valtozo);
 
-var_dump($associative_array);
 
-$index_array []= 6;
 
+# Típusok
+// megszokott műveletek <, <=, >, >=, ==, !=, ===, !== (szigorú egyenlőségvizsgálatok)
+
+## szám
+// kétféle egész és dupla precizitású float
+$intVal = 1_000_000;
+$floatVal = 3.1_4;
+// számokat _-al lehet tagolni
+// int túlcsordulás -> float konverzió
+// megszokott aritmetika opreátorok +, -, *, /, %, összevont értékadó operátorok, ++, --
+var_dump(intdiv($intVal, 12)); // egész osztás
+var_dump($intVal**2); // hatványozás
+
+
+## string
+$str = '$valtozo';
+$strSub = "{$valtozo}dsdas";
+$concat = $str . $strSub; // összefűzés
+// `` shell commandként hajta végre a megadott szöveget -> ne használjuk
+// műveletek általában str-el vannak prefixálva
+var_dump(strlen($str));
+
+
+## bool
+$trueVal = true;
+$falseVal = false;
+// php fura típuskonverziókat tud végezni pl. "0" -> false
+
+
+## speciális érték null (logikai viszgálatnál false)
+$nullVal = null;
+
+
+## array
+// valójában asszociatív tömb (mint JS-be az objektum)
+// fontos különbség, hogy paraméterátadásnál primitív értékként viselkednek (másolódnak)
+// akár több típusú kulcs és érték is lehet
+// array, objektum, függvény nem lehet kulcs
+// általában int és string
+// tetszőleges mértékben egymásba ágyazhatók
+$assoc_array = [
+    null => "asdasd",
+    false => "asdsad",
+//    3.14 => "asdasd", // float -> int, ha kulcs
+    3 => "asdasdasd",
+    'elso' => [1 => "asd", "sd" => 5],
+    1 => [1, 2, 3],
+    5 // kulcs nélkül (ekkor a kulcs a legnagyobb int kulcs rákövetkezője)
+];
+
+var_dump($assoc_array);
+
+// ha nincs kulcs sima index alapú tömb
+$index_array = [1, 2, 3, 4, 5];
 
 var_dump($index_array);
 
-$associative_array['negyedik'] = "kasda";
+// érték beállítása
+$assoc_array['harmadik'] = "asdasdsd";
 
-var_dump($associative_array);
+// ha nem adunk meg semmit akkor a legnagyobb int kulcs rákövetkezője lesz az új kulcs
+$assoc_array[] = 'asdasdasd';
+
+// tömb hossz
+var_dump(count($assoc_array));
+
+// tömb műveletek általában array-el prefixált függvények
+// viszonylag beszédes nevek
+var_dump(array_reduce($index_array, fn($a, $b) => $a + $b, 0));
+var_dump(array_sum($index_array));
+var_dump(array_map(fn($num) => $num ** 2, $index_array));
+var_dump(array_unique($index_array));
 
 
-// hossz
-var_dump(count($associative_array));
+## Objektum
+// stdClass-től minden implicit módon örököl
+// Java-hoz nagyon hasonló, kivéve package private láthatóság
+// adattag elérés opretátor ->
+abstract class Obj extends stdClass implements asd {
+    public int $asd;
+    private $sadasda;
+    protected $asdasd;
 
-// array érték szerint
-function editArray(&$array) {
-    $array['elso'] = -1;
+    // túlterhelés nincs
+    public function __construct(int $asd) {
+        $this->asd = $asd;
+    }
+
+    // __ = "magic methods"
+    public function __toString(): string {
+        return "asd";
+    }
+
+    // ? lehet null, | megadott típusok közül az egyik
+    public abstract function asd(?int $asd, string|int $asd2): string;
 }
 
-editArray($associative_array);
-
-var_dump($associative_array);
-
-
-for ($i = 0; $i < 5; $i++) {
+interface asd {
 
 }
+
+
+
+## Statikus típusosság
+// az objektumok adattagjai illetve a függvények típusinformációval egészíthetőek ki, lásd előző osztálydefiníció
+// speciális típusok
+// void - nincs visszatérés
+// never - függvény soha nem áll le
+// mixed - az összes alap típus egyike
+// callable - függvény neve vagy implementál az __invoke magic method-ot
+
+
+
+## Függvények
+// 3 típus
+
+### klasszikus
+function inc(int $a): int {
+    return $a + 1;
+}
+
+// átadás paraméterként: függvény neve mint string
+var_dump(array_map('inc', $index_array));
+
+
+### lambda
+// Closure típusú objektumok
+$inc2 = function(int $a) {
+    return $a + 1;
+};
+
+var_dump($inc2);
+var_dump(array_map($inc2, $index_array));
+
+
+### egysoros lambda
+// Closure típusú objektumok
+$inc3 = fn(int $a) => $a + 1;
+
+var_dump($inc3);
+var_dump(array_map($inc3, $index_array));
+
+
+### zártság
+// külső scope-ból csak úgy tudunk adatot elérni, ha azt explicit elmondjuk a PHP-nak
+// kivétel szuperglobális változók https://www.php.net/manual/en/language.variables.superglobals.php
+
+$b = 5;
+
+#### sima függvény esetén törzsön belül global
+function a(int $a): int {
+    global $b;
+    return $a + $b;
+}
+
+#### lambda esetén use()
+$a2 = function(int $a) use ($b) {
+    return $a + 1;
+};
+// egysoros lambdánál nem működik
+
+
+
+## Tömbök paraméterátadása
+// érték szerint
+// &-el megadható, hogy egy paraméter mindig referencia szerint adódjon át
+function modify_array(array &$arr, mixed $key, mixed $value): void {
+    $arr[$key] = $value;
+}
+
+$arr2 = [1, 2, 3];
+
+// értékadásnál is tudunk refereenciákat használni
+$arr3 = &$arr2;
+
+var_dump($arr2);
+
+modify_array($arr3, 2, 4);
+
+var_dump($arr2);
+
+
+# vezérlési szerkezetek
+
+## elágazás
+if (true) {
+
+} elseif (false) {
+
+} else if (false) {
+
+} else {
+
+}
+
+## switch
+switch ($str) {
+    case 'a':
+        echo 'a';
+    case 'fallthrough':
+        echo 'fall';
+        break;
+    case 'b':
+        echo 'b';
+        break;
+    default:
+        echo 'else';
+        break;
+}
+
+## while
+while (true) {
+    break;
+}
+
+## do-while
+do {
+    continue;
+} while(false);
+
+## for
+
+for ($i = 0; $i < 10; $i++) {
+
+}
+
+## foreach
 
 foreach ($index_array as $item) {
 
 }
 
-foreach ($associative_array as $index => $item) {
+## kulcs érték foreach
+foreach ($assoc_array as $index => $item) {
 
 }
 
+# alternatív szintaxis
+// minden vezérlési szerkezet leírható így is
 
-$newItem = 5;
+if (true):
 
-$n = 5;
+elseif (false):
 
-$exponent = function(int $num) use ($n) {
-    return $num ** $n;
-};
+else:
 
-$square = fn(int $num) => $num ** 2;
+endif;
 
-var_dump($exponent(5));
-
-var_dump(array_reduce($index_array, fn($a, $b) => $a + $b, 0));
-var_dump(array_sum($index_array));
-var_dump(array_map(fn($num) => $num ** 2, $index_array));
-
-var_dump(array_filter($index_array, fn($num) => $num % 2 == 0));
-
-
+// ez azért jó mert így a php kódunkat HTML kód közé tudjuk ékelni
--- 07/index.php
+++ 07/index.php
@@ -1,89 +1,92 @@
 <?php
-$errors = ['A', 'B', 'C', 'D', 'E'];
-$categories = [5 => "Akció", 4 => "Dráma", 8 => "Vígjáték"];
-$products = [23 => "Pendrive", 15 => "HDD", 12 => "SSD", 9 => "RAM"];
+// először a basics.php-ba nézz be
 
-class Question {
-}
+# Sablonprogramozás
+// PHP HTML-be ékelhető
+// ezzel egy ún. sablon jön létre, melynek bizonyos részeire
+//  értéket helyetessítünk
+//  feltételesen generálunk le
+//  ciklusban generálunk le
+// feladatunk rájönni mely részek egyeznek meg és hol kell behelyettesíteni
 
-$questions = [
-//        5 => new Question('Első kérdés szövege.', ['A', 'B', 'C'], [2]),
-//        6 => new Question('Második kérdés szövege.', ['A', 'B', 'C', 'D'], [0, 1])
-];
-
-$students = [
-    ["name" => "Peter Kovacs", "neptun" => "ABC123", "birth_year" => 1990, "gender" => "Male"],
-    ["name" => "Anna Toth", "neptun" => "DEF456", "birth_year" => 1985, "gender" => "Female"],
-    ["name" => "Gabor Nagy", "neptun" => "GHI789", "birth_year" => 1972, "gender" => "Male"],
-    ["name" => "Emese Kiss", "neptun" => "JKL012", "birth_year" => 1995, "gender" => "Female"],
-    ["name" => "Adam Szabo", "neptun" => "MNO345", "birth_year" => 1998, "gender" => "Male"],
-];
-
-
-function fact(int $num): int {
-    if ($num < 0) {
-        return 0;
-    }
-
-    $prod = 1;
-    for ($i = 2; $i <= $num; $i++) {
-        $prod *= $i;
-    }
-    return $prod;
-}
+/* <?php és ?> közé írtak php-ként kerülnek értelmezésre */
+/* <?= ... ?> ugyan az mint <?php echo ... ?> */
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Webprogramozás 7. gyakorlat</title>
+    <title>7. gyakorlat</title>
 </head>
 <body>
-<h1>Webprogramozás 7. gyakorlat</h1>
+<h1>7. gyakorlat</h1>
 
 <div>
-    <h2>Első feladat</h2>
-    <h3><?php echo "Helló!" ?></h3>
-    <h3><?= "Helló!" ?></h3>
+    <h2>1. feladat</h2>
+    <?php $nev = "Bálint" ?>
+    <?php echo "<h3>$nev</h3>" ?>
+    <?= "<h3>$nev</h3>" ?>
+    <!--  De igazából ennyi is elég. Mindig fontos azt azonosítani, hogy mi változik és mi nem.  -->
+    <h3>Helló <?= $nev ?>!</h3>
 </div>
 
 <div>
-    <h2>Második feladat</h2>
+    <h2>2. feladat</h2>
+    <?= date("Y-m-d H:i:s"); ?>
 </div>
 
 <div>
-    <h2>Harmadik feladat</h2>
-    <?= fact(5) ?>
+    <h2>3. feladat</h2>
+    <?php
+
+    function factorial(int $n): int {
+        if ($n < 2) {
+            return 1;
+        }
+
+        return $n * factorial($n - 1);
+    }
+
+    ?>
+    <div><?= factorial(1) ?></div>
+    <div><?= factorial(2) ?></div>
+    <div><?= factorial(3) ?></div>
+    <div><?= factorial(4) ?></div>
+    <div><?= factorial(5) ?></div>
 </div>
 
 <div>
-    <h2>Negyedik feladat</h2>
+    <h2>4. feladat</h2>
+    <!-- Vezérlési szerkezeteket is tudunk közbeékelni. -->
     <?php for ($i = 0; $i < 10; $i++): ?>
-        <h3 style='font-size: <?= 1 + $i * 0.25 ?>rem'><?= $i ?></h3>
+        <h3 style='font-size: <?= 1 + $i * 0.25 ?>rem'>Helló világ!</h3>
     <?php endfor; ?>
 </div>
 
 <div>
-    <h2>Ötödik feladat</h2>
+    <h2>5. feladat</h2>
+    <?php $errors = ['A', 'B', 'C', 'D', 'E']; ?>
     <ol>
-    <?php foreach ($errors as $error): ?>
-        <li><?= $error ?></li>
-    <?php endforeach; ?>
+        <?php foreach ($errors as $error): ?>
+            <li><?= $error ?></li>
+        <?php endforeach; ?>
     </ol>
 </div>
 
 <div>
-    <h2>Hatodik feladat</h2>
+    <h2>5. feladat</h2>
+    <?php $categories = [5 => "Akció", 4 => "Dráma", 8 => "Vígjáték"]; ?>
     <select>
-    <?php foreach ($errors as $categoryId => $categoryName): ?>
-        <option value='<?= $categoryId ?>'><?= $categoryName ?></option>
-    <?php endforeach; ?>
+        <?php foreach ($categories as $id => $name): ?>
+            <option value='<?= $id ?>'><?= $name ?></option>
+        <?php endforeach; ?>
     </select>
 </div>
 
 <div>
-    <h2>Hetedik feladat</h2>
+    <h2>7. feladat</h2>
+    <?php $products = [23 => "Pendrive", 15 => "HDD", 12 => "SSD", 9 => "RAM"]; ?>
     <form>
         <?php foreach($products as $productId => $productName): ?>
             <input id='<?= $productName ?>' type='checkbox' name='products' value='<?= $productId ?>'/>
@@ -96,39 +99,42 @@ function fact(int $num): int {
 </div>
 
 <div>
-    <h2>Nyolcadik feladat</h2>
-</div>
-<div>
-    <h2>Kilencedik feladat</h2>
+    <h2>8. feladat</h2>
+    <?php
+
+    $students = [
+        ["name" => "Peter Kovacs", "neptun" => "ABC123", "birth_year" => 1990, "gender" => "Male"],
+        ["name" => "Anna Toth", "neptun" => "DEF456", "birth_year" => 1985, "gender" => "Female"],
+        ["name" => "Gabor Nagy", "neptun" => "GHI789", "birth_year" => 1972, "gender" => "Male"],
+        ["name" => "Emese Kiss", "neptun" => "JKL012", "birth_year" => 1995, "gender" => "Female"],
+        ["name" => "Adam Szabo", "neptun" => "MNO345", "birth_year" => 1998, "gender" => "Male"],
+    ];
+
+    ?>
     <?php if (count($students) === 0): ?>
-        <p>Nincs diák</p>
+        <div>Nincs tanuló!</div>
     <?php else: ?>
         <table>
             <thead>
-                <tr>
-                    <th>Név</th>
-                    <th>Neptun</th>
-                    <th>Születés</th>
-                    <th>Nem</th>
-                </tr>
+            <tr>
+                <th>Név</th>
+                <th>Neptun</th>
+                <th>Születés</th>
+                <th>Nem</th>
+            </tr>
             </thead>
             <tbody>
-            <?php foreach ($students as $student): ?>
-                <tr>
-                    <td><?= $student['name'] ?></td>
-                    <td><?= $student['neptun'] ?></td>
-                    <td><?= $student['birth_year'] ?></td>
-                    <td><?= $student['gender'] ?></td>
-                </tr>
-            <?php endforeach; ?>
+                <?php foreach ($students as $student): ?>
+                    <tr>
+                        <td><?= $student['name'] ?></td>
+                        <td><?= $student['neptun'] ?></td>
+                        <td><?= $student['birth_year']  ?></td>
+                        <td><?= $student['gender'] ?></td>
+                    </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
     <?php endif; ?>
-
-
-</div>
-<div>
-    <h2>Tizedik feladat</h2>
 </div>
 </body>
 </html>
